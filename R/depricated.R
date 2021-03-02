b7e0306b0948197a9320ccb934d82bbea22d6220
bbartholdy <bbartholdy@github.com>
2021-03-02 16:22:24 +0100

    add pkgdown site

--- R/depricated.R
+++ R/depricated.R
@@ -1,6 +1,32 @@
-# depricated functions
+#' Age-at-death estimation
+#'
+#' This function can be used to estimate age-at-death with the choice of using Middenbeemster as a reference population or incorporating a sample from the R-evnironment.
 #' \lifecycle{depricated}
-#' @export
+#' @param wear Numeric. The wear score of an individual. Can be a single value or a vector containing a list of wear scores from multiple individuals.
+#' @param data Data frame containing one column for ages-at-death and a second column for the average wear scores. Not required when pop = "MB11" is selected.
+#' @param rank Numeric. Indicates the rank of the polynomial regression. 1 = linear, 2 = quadratic, etc. Default is 2.
+#' @param pop Character. Indicates which reference population to use. "MB11" and "other" are supported. When "other" is selected, the data input is required.
+#' @param interval Character. The type of age interval used. Can be either "prediction" or "confidence" intervals. The default is set at "prediction".
+#' @param level Numeric. Determines the level of confidence or prediction intervals. Is a number between 0 and 1 (not inclusive). 0.68, 0.90, or 0.95 is recommended. The default is set at 0.68.
+#' @param mars.int Logical. Apply MARS-sized prediction intervals.
+#' @param ... Can be used to pass additional arguments to the lm function.
+#' @details This function uses the "lm" function for the regression analyses.
+#'
+#' @return a data frame containing the following values:
+#' \describe{
+#'  \item{\code{wear}}{The wear score(s) initially applied to the function}
+#'
+#'  \item{\code{estimate}}{Age-at-death estimate(s) calculated from the wear score(s)}
+#'
+#'  \item{\code{range}}{the age range (+- years) of the estimate, determined by the type and level.}
+#'
+#'  \item{\code{lower}}{the lower bound of the age interval.}
+#'
+#'  \item{\code{upper}}{the upper bound of the age interval.}
+#' }
+#' @importFrom stats lm na.omit predict
+#' @example inst/BAMSAURex.R
+#' @keywords Age-at-death estimation
 BAMSAUR <- function(wear, data = NULL, rank = 2, pop = "MB11", class.cal = F, interval = "prediction", level = 0.68, mars.int = F, ...){
 warning("`BAMSAUR()` is deprecated as of BAMSAUR 0.1.0. Please use `bamsaur()` instead")
   wear <- as.data.frame(wear)
